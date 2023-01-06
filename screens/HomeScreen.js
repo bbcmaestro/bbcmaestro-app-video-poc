1c12bda4173e2f29cbccfce5f2cfced58c165064
Chris McKirgan <chris@mckirgan.com>
2023-01-06 12:32:45 +0000

    feat: adds example usedimension issue

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,28 +1,61 @@
 import React from 'react';
-import { SafeAreaView, Button } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
+import {
+  SafeAreaView,
+  Button,
+  useWindowDimensions,
+  View,
+  Text,
+} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
 import * as ScreenOrientation from 'expo-screen-orientation';
 
 const HomeScreen = () => {
   const navigation = useNavigation();
+  const {height, width} = useWindowDimensions();
 
   ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.PORTRAIT_UP);
 
-  const playVideo = (uri) => {
-    navigation.navigate('Video', { uri: uri });
-  }
+  const playVideo = uri => {
+    navigation.navigate('Video', {uri: uri});
+  };
 
   return (
     <SafeAreaView>
       <Button
-        title='🐇 Play Big Buck Bunny 🐇'
-        onPress={() => playVideo('https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4')}
+        title="🐇 Play Big Buck Bunny 🐇"
+        onPress={() =>
+          playVideo(
+            'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4',
+          )
+        }
       />
 
       <Button
-        title='🐘 Play Elephants Dream 🐘'
-        onPress={() => playVideo('https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4')}
+        title="🐘 Play Elephants Dream 🐘"
+        onPress={() =>
+          playVideo(
+            'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4',
+          )
+        }
       />
+      <Text
+        style={{
+          marginBottom: 20,
+        }}>
+        Width: {width}, Height: {height}
+      </Text>
+      <View
+        style={{
+          width,
+          backgroundColor: '#eeccee',
+        }}>
+        <Text>
+          This text should fill the entire screen and not exceed boundaries.
+          This text should fill the entire screen and not exceed boundaries.
+          This text should fill the entire screen and not exceed boundaries.
+          This text should fill the entire screen and not exceed boundaries.
+        </Text>
+      </View>
     </SafeAreaView>
   );
 };
